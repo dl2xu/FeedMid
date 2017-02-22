e26cdcb4dc61d66770a54a6e9c5690323693d21e
hthkwan <herman_fire@hotmail.com>
2017-02-21 21:47:03 -0700

    3rd init commit
    well, here's the 3rd  "init" commit

---
+++ app/src/main/java/com/hackathon/feedmid/feedmid/Window2.java
@@ -0,0 +1,27 @@
+package com.hackathon.feedmid.feedmid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Window2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_window2);
+
+        Button back = (Button) findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                finish();
+            }
+        });
+
+    }
+
+    public void onClick(){
+        finish();
+    }
+}
